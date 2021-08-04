f77a55200f831fb62c181b0000caaa43e1aae608
clawy <raina_cally_ang@dlsu.edu.ph>
2021-08-04 21:44:18 +0800

    updated minigame1, changed timer prefab

--- Edugame Project/Assets/Scripts/Minigame1TestScript.cs
+++ Edugame Project/Assets/Scripts/Minigame1TestScript.cs
@@ -8,33 +8,69 @@ public class Minigame1TestScript : MonoBehaviour
     bool isPlaying = true; // is minigame playing
 
     [SerializeField] Slider slider;
-    [SerializeField] Slider timerSlider;
 
-    float valueTicks = 0.0f;
-    [SerializeField] float valueTimer = 2.0f; // time before slider value decreases
+    enum MinigameState { WALK, BRISKWALK, RUN}
+    MinigameState currentState = MinigameState.WALK;
 
     float ticks = 0.0f;
+
+    [SerializeField] float walkMinTime = 1.5f;
+    [SerializeField] float walkMaxTime = 2.0f;
+
+    [SerializeField] float briskWalkMinTime = 1.0f;
+    [SerializeField] float briskWalkMaxTime = 1.5f;
+
+    [SerializeField] float runMinTime = 0.5f;
+    [SerializeField] float runMaxTime = 1.0f;
+
+    float interval = 0.0f; // time before the value decreases
+
+    float stateTicks = 0.0f;
+    [SerializeField] float stateInterval = 3.0f; // time before the state changes
+
+    [SerializeField] Timer timer;
+
+    private void Start()
+    {
+        this.GenerateRandomIntervalValue();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (this.isPlaying)
         {
-            this.valueTicks += Time.deltaTime;
-            this.ticks += Time.deltaTime;
-            if (this.valueTicks >= this.valueTimer)
+            // check for failure condition
+            if (slider.value >= slider.maxValue || slider.value <= 0) // value reached extreme sides
             {
-                slider.value--;
-                this.valueTicks = 0;
+                Debug.Log("Lose :(");
+                this.isPlaying = false;
+                this.timer.isPlaying = false;
             }
-            if (this.ticks >= 1.0f)
+            if (timer.timeSlider.value <= 0)
             {
-                this.timerSlider.value--;
+                this.isPlaying = false;
+                this.timer.isPlaying = false;
+                this.CheckResult();
+            }
+
+            // slider value timer
+            this.ticks += Time.deltaTime;
+            if (this.ticks >= this.interval)
+            {
                 this.ticks = 0;
-                if (this.timerSlider.value == 0)
+                this.slider.value--;
+                this.GenerateRandomIntervalValue();
+            }
+
+            // minigame state timer
+            if (this.currentState != MinigameState.RUN)
+            {
+                this.stateTicks += Time.deltaTime;
+                if (this.stateTicks >= this.stateInterval)
                 {
-                    this.CheckResult();
-                    this.isPlaying = false;
+                    this.stateTicks = 0;
+                    this.currentState++; 
                 }
             }
         }
@@ -42,7 +78,10 @@ public class Minigame1TestScript : MonoBehaviour
 
     public void OnButtonClick()
     {
-        slider.value++; 
+        if (this.isPlaying)
+        {
+            this.slider.value++;
+        }
     }
 
     void CheckResult()
@@ -54,6 +93,26 @@ public class Minigame1TestScript : MonoBehaviour
         else
         {
             Debug.Log("Lose :(");
+        }
+    }
+
+    // randomly generate a value for interval variable
+    void GenerateRandomIntervalValue()
+    {
+        if (this.currentState == MinigameState.WALK)
+        {
+            this.interval = Random.Range(this.walkMinTime, this.walkMaxTime);
+            Debug.Log("Current state: WALK");
+        }
+        else if (this.currentState == MinigameState.BRISKWALK)
+        {
+            this.interval = Random.Range(this.briskWalkMinTime, this.briskWalkMaxTime);
+            Debug.Log("Current state: BRISK WALK");
+        }
+        else if (this.currentState == MinigameState.RUN)
+        {
+            this.interval = Random.Range(this.runMinTime, this.runMaxTime);
+            Debug.Log("Current state: RUN");
         }
     }
 }
