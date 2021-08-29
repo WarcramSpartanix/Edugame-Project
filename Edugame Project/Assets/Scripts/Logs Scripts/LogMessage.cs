18219ff4ba350c852a7d8305888aa5168d3bcfbb
clawy <raina_cally_ang@dlsu.edu.ph>
2021-08-29 19:55:46 +0800

    ui revisions

--- Edugame Project/Assets/Scripts/Logs Scripts/LogMessage.cs
+++ Edugame Project/Assets/Scripts/Logs Scripts/LogMessage.cs
@@ -2,14 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+
+public enum LogType { NONE, POOR, ADEQUATE, EXCELLENT}
 
 public class LogMessage : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI logText;
+    [SerializeField] Image emojiImage; 
 
-    public void AddMessage(string message)
+    [SerializeField] Sprite poorEmoji;
+    [SerializeField] Sprite adequateEmoji;
+    [SerializeField] Sprite excellentEmoji;
+
+    public void AddMessage(string message, LogType type)
     {
         this.logText = GetComponentInChildren<TextMeshProUGUI>();
         this.logText.text = message;
+
+        switch (type)
+        {
+            case LogType.POOR:
+                this.emojiImage.sprite = poorEmoji;
+                break;
+
+            case LogType.ADEQUATE:
+                this.emojiImage.sprite = adequateEmoji;
+                break;
+
+            case LogType.EXCELLENT:
+                this.emojiImage.sprite = excellentEmoji;
+                break;
+        }
     }
 }
